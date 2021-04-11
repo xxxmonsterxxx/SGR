5024054dac29007488338c28052460799032c6d7
Mikhail <arsenal-mihail@mail.ru>
2021-04-11 23:22:18 +0300

    Git init. Created renderer class template. Added window manager class.

---
+++ SGR.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// Simple Graphic Renderer (SGR)
+// Mikhail Polestchuk, 2021, april.
+
+#include <GLFW/glfw3.h>
+
+class SGR {
+
+};
---
+++ WindowManager.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "GLFW/glfw3.h"
+
+class WindowManager {
+
+private:
+
+public:
+	WindowManager() {};
+	~WindowManager() {};
+
+};

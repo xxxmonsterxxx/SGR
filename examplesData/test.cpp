104996b483eeb15e0463346da4827efd4010dc51
Mikhail Polestchuk <arsenal-mihail@mail.ru>
2022-07-16 20:47:13 +0300

    Added example for show possibility to render text.

--- examplesData/test.cpp
+++ examplesData/test.cpp
@@ -1,6 +1,66 @@
 #include <SGR.h>
 #include <iostream>
 
+#include <stdio.h>
+
+#define STB_TRUETYPE_IMPLEMENTATION
+	#include "../modules/stb/stb_truetype.h"
+
+bool getFontData(unsigned char* &fontPixels, stbtt_bakedchar* &backedChars, uint32_t &width, uint32_t &height)
+{
+	FILE* font_ttf = fopen("Resources/fonts/times new roman.ttf", "rb");
+	if (!font_ttf) {
+		printf("Font file not found! :(\n");
+		return false;
+	}
+
+	long file_size;
+	if (fseek(font_ttf, 0, SEEK_END) !=0) {
+		printf("File is corrupted\n");
+		return false;
+	} else {
+		file_size = ftell(font_ttf);
+		fseek(font_ttf, 0, SEEK_SET);
+	}
+
+	unsigned char* font_data = (unsigned char*)malloc(file_size);
+	if(fread(font_data, 1, file_size, font_ttf) != file_size) {
+		printf("Real time corrupted file\n");
+		return false;
+	};
+	fclose(font_ttf);
+
+	int bitmapWidth = 512;
+	int bitmapHeight = 512;
+	width = bitmapWidth;
+	height = bitmapHeight;
+	fontPixels = (unsigned char*)malloc(bitmapWidth*bitmapHeight);
+	unsigned char* fontPixels1 = (unsigned char*)malloc(bitmapWidth*bitmapHeight);
+	unsigned char fontPixels2[512*512];
+	const int char_number_to_bake = 96;
+	backedChars = (stbtt_bakedchar*)malloc(char_number_to_bake*sizeof(stbtt_bakedchar));
+
+	int result = stbtt_BakeFontBitmap(font_data, 0, 64, fontPixels, bitmapWidth, bitmapHeight, 32, char_number_to_bake, backedChars);
+	free(font_data);
+
+	return true;
+}
+
+
+void getLetterFontData(char letter, stbtt_bakedchar* fontData, glm::vec2& fontCoord, glm::vec2& fontSize)
+{
+	stbtt_aligned_quad charData;
+	float xstart, ystart;
+	stbtt_GetBakedQuad(fontData, 512, 512, (uint32_t)letter - 32, &xstart, &ystart, &charData, 1);
+
+	float xFont = (charData.s1-charData.s0)*0.5f;
+	float yFont = (charData.t1-charData.t0)*0.5f;
+
+	fontCoord = glm::vec2(charData.s0+xFont,charData.t0+yFont);
+	fontSize = glm::vec2(charData.s1-charData.s0,charData.t1-charData.t0);
+}
+
+
 float getSgrTimeDuration(SgrTime_t start, SgrTime_t end)
 {
 	return std::chrono::duration<float, std::chrono::seconds::period>(end - start).count();
@@ -129,10 +189,8 @@ int main()
 	// new object layout creating done. Next step - setup this data to SGR and add command to draw.
 
 	std::vector<VkDescriptorSetLayoutBinding> setLayoutBinding = createDescriptorSetLayoutBinding();
-
 	std::vector<VkVertexInputBindingDescription> bindInpDescr = createBindingDescr();
 	std::vector<VkVertexInputAttributeDescription> attDescr = createAttrDescr();
-
 
 	struct InstanceData {
 		glm::mat4 model;
@@ -141,7 +199,7 @@ int main()
 		glm::vec3 color;
 	};
 
-	rectangles.instnaceCount = 3;
+	rectangles.instnaceCount = 4;
 	rectangles.instanceSize = sizeof(InstanceData);
 	MemoryManager::createDynamicUniformMemory(rectangles);
 	SgrBuffer* instanceUBO = nullptr;
@@ -170,6 +228,13 @@ int main()
 	if (resultCreateTextureImage != sgrOK)
 		return resultCreateTextureImage;
 
+	SgrImage* textImage = nullptr;
+	unsigned char* fontPixels = nullptr;
+	stbtt_bakedchar* fontData;
+	uint32_t pixelsWidth, pixelsHeight;
+	getFontData(fontPixels, fontData, pixelsWidth, pixelsHeight);
+	resultCreateTextureImage = TextureManager::createFontTextureImage(fontPixels, pixelsWidth, pixelsHeight, textImage);
+
 	sgr_object1.addObjectInstance("man","rectangle",0*rectangles.dynamicAlignment);
 
 	std::vector<void*> objectData;
@@ -197,8 +262,6 @@ int main()
 	objectData2.push_back((void*)(instanceUBO));
 	sgr_object1.writeDescriptorSets("tree", objectData2);
 
-	if (sgr_object1.drawObject("tree") != sgrOK)
-		return 200;
 
 	//-------
 
@@ -211,11 +274,26 @@ int main()
 	sgr_object1.writeDescriptorSets("road", objectData3);
 
 
+	sgr_object1.addObjectInstance("LetterH", "rectangle", 3*rectangles.dynamicAlignment);
+
+	std::vector<void*> objectData4;
+	objectData4.push_back((void*)(uboBuffer));
+	objectData4.push_back((void*)(textImage));
+	objectData4.push_back((void*)(instanceUBO));
+	sgr_object1.writeDescriptorSets("LetterH", objectData4);
+
+
 	if (sgr_object1.drawObject("man") != sgrOK)
 		return 100;
 
+	if (sgr_object1.drawObject("tree") != sgrOK)
+		return 200;
+
 	if (sgr_object1.drawObject("road") != sgrOK)
-		return 100;
+		return 300;
+
+	if (sgr_object1.drawObject("LetterH") != sgrOK)
+		return 400;
 
 	//------------
 
@@ -242,7 +320,7 @@ int main()
 
 	model = (glm::mat4*)((uint64_t)rectangles.data + 2*rectangles.dynamicAlignment);
 	*model = glm::mat4(1.f);
-	*model = glm::translate(*model,glm::vec3(0,0,-3));
+	*model = glm::translate(*model,glm::vec3(-1,0,-5));
 	*model = glm::scale(*model,glm::vec3(1.f,1.f,1.f));
 	texCoord = (glm::vec2*)((uint64_t)rectangles.data + sizeof(glm::mat4) + 2*rectangles.dynamicAlignment);
 	texCoord->x = 0.5f;
@@ -250,6 +328,19 @@ int main()
 	texSize = (glm::vec2*)((uint64_t)rectangles.data + sizeof(glm::vec2) + sizeof(glm::mat4) + 2*rectangles.dynamicAlignment);
 	texSize->x = 2.f;
 	texSize->y = 2.f;
+
+	model = (glm::mat4*)((uint64_t)rectangles.data + 3*rectangles.dynamicAlignment);
+	*model = glm::mat4(1.f);
+	*model = glm::translate(*model,glm::vec3(1,0,-2));
+	*model = glm::scale(*model,glm::vec3(1.f,1.f,1.f));
+	texCoord = (glm::vec2*)((uint64_t)rectangles.data + sizeof(glm::mat4) + 3*rectangles.dynamicAlignment);
+	glm::vec2 fontC,fontS;
+	getLetterFontData('s', fontData, fontC,fontS);
+	texCoord->x = fontC.x;
+	texCoord->y = fontC.y;
+	texSize = (glm::vec2*)((uint64_t)rectangles.data + sizeof(glm::vec2) + sizeof(glm::mat4) + 3*rectangles.dynamicAlignment);
+	texSize->x = fontS.x*2;
+	texSize->y = fontS.y*2;
 
 	sgr_object1.updateInstancesUniformBufferObject(rectangles);
 	ubo.view = glm::translate(ubo.view, glm::vec3(0, 0, -1));

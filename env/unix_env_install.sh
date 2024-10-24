8cb4c15f6d1c3cf469d46fc3d662dfa7480ec892
Mikhail Polestchuk <arsenal-mihail@mail.ru>
2024-10-24 20:12:49 +0300

    Also sudo removed git actions.

--- env/unix_env_install.sh
+++ env/unix_env_install.sh
@@ -191,9 +191,9 @@ else
         if [ $SYSTEM_TYPE == Linux ]
         then
             wget -qO- https://packages.lunarg.com/lunarg-signing-key-pub.asc | sudo tee /etc/apt/trusted.gpg.d/lunarg.asc
-            sudo wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
-            sudo apt update
-            sudo apt install vulkan-sdk
+            wget -qO /etc/apt/sources.list.d/lunarg-vulkan-jammy.list http://packages.lunarg.com/vulkan/lunarg-vulkan-jammy.list
+            apt update
+            apt install vulkan-sdk
         fi
 
         echo "3b. Checking Vulkan"
